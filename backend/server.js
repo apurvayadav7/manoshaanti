5d1da53c18cd65a4b01ce0c608539875488ef280
VAIBHAV SHARMA <vaibhav2_cseaiml25@delhitechnicalcampus.ac.in>
2026-06-12 17:34:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 const chatRoutes = require('./routes/chatRoutes');
